637f395200d54a0e2fb55deb1bf7bd54865a30a3
Sargy <lukina_sargy@mail.ru>
2019-02-21 12:03:10 +0300

    3d

--- source/index.js
+++ source/index.js
@@ -1,5 +1,5 @@
 import createMenu from './menu';
-var menu = createMenu(['Главная','О нас','Отзывы','Контакты'], 'menu');
+var menu = createMenu(['Главная','О нас','Отзывы'], 'menu');
 document.body.appendChild(menu);
 var script = document.createElement("script");
 script.setAttribute('src','http://localhost:35729/livereload.js');
